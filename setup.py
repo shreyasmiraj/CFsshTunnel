3121dcf16101b8d4d9dc9d5184cacf3f3c03a860
ThePilot <shreyasmiraj8@gmail.com>
2022-02-07 10:44:31 +0530

    Updated version to 0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setup(
     name='CFsshTunnel',
     packages=find_packages(),
-    version='0.5',
+    version='0.6',
     license='MIT',
     description='A light weight open-ssh server tunnel through cloudflare free tunnel service',
     long_description=long_description,
