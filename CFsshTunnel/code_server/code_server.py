01e126dc66ca0b089b15074c0c3a8bc1fb02f327
ThePilot <shreyasmiraj8@gmail.com>
2022-02-14 20:19:38 +0530

    minor improvement

--- CFsshTunnel/code_server/code_server.py
+++ CFsshTunnel/code_server/code_server.py
@@ -67,6 +67,6 @@ def launch_codeserver(user:str,
     subprocess.Popen(codeserver_command.split(" "))
     time.sleep(5)
     #port forward client localhost to remote instance
-    print("\nPort forward client localhost:" + str(client_port) + "onto the remote code-server instance through ssh as follows(run this on your client device): ")
+    print("\nPort forward client localhost:" + str(client_port) + " onto the remote code-server instance through ssh as follows(run this on your client device): ")
     seperator_command_border("$ ssh -N -L "+ str(client_port) + ":127.0.0.1:" + str(server_port) + " " + user + "@" + hostname + " " + "&")
     box_border("Codeserver is now accessible at: http://localhost:" + str(client_port) +"\n Note: localhost cannot provide SSL, so use http instead of https")
